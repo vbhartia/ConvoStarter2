08fbcf9626fdf7308dc74664ecbc70862b8f9724
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-10-05 20:56:17 -0400

    syncronization not working

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -3,52 +3,51 @@ class ConnectController < ApplicationController
 	end
 
 	def start_connect
-		puts params[:coords][:latitude]
+    	Connection.uncached do 
+			puts params[:coords][:latitude]
 
-		#Record Connection
-		connection = Connection.create()
-    	connection.user = current_user
-    	connection.lat = params[:coords][:latitude]
-    	connection.long = params[:coords][:longitude]
-    	connection.save
+			#Record Connection
+			connection = Connection.create()
+	    	connection.user = current_user
+	    	connection.lat = params[:coords][:latitude]
+	    	connection.long = params[:coords][:longitude]
+	    	connection.save
 
-		#Find Matches	
-    	time_range = (Time.now - 5.seconds)..Time.now
-		possible_connections = Connection.where('created_at' => time_range)
-		possible_connections = possible_connections.delete_if{|x| x.id == connection.id}	
+	    	time_range = (Time.now - 10.seconds)..Time.now
+			returned_possible_connections = Connection.where('created_at' => time_range)
+			returned_possible_connections = returned_possible_connections.delete_if{|x| x.id == connection.id}	
 
-    	possible_connections.each do |indv_connection|
-    		puts indv_connection.created_at
-    	end 
-
-    	puts possible_connections.count
-
-    	#Check location
+	    	#Check location
 
 
-    	#Connection Made
-		#@connected_user = possible_connections.first.user
+	    	#Connection Made
+			#@connected_user = possible_connections.first.user
 
-		if (possible_connections.count > 0)
-		    
-		    #sleep(5)
+			if (returned_possible_connections.count > 0)
+			    respond_to do |format|
+	    		    format.json { render :json => user_pages_url }
+	    		end
+			else
+				sleep 5
+		    	new_time_range = (Time.now - 10.seconds)..Time.now
+				
+	    	
+				new_possible_connections = Connection.where('created_at' => new_time_range)
+				new_possible_connections = new_possible_connections.delete_if{|x| x.id == connection.id}	
 
-
-
-		    respond_to do |format|
-    		    format.json { render :json => user_pages_url }
-    		end
-
-		else
-			respond_to do |format|
-    		    format.json { render :json => static_none_found_url }
-    		end
+				if (new_possible_connections.count > 0)
+				    respond_to do |format|
+		    		    format.json { render :json => user_pages_url }
+		    		end
+		    	else
+					respond_to do |format|
+	    			    format.json { render :json => static_none_found_url }
+	    			end
+	    		end
+	    		
+			end
 		end
 	end
+
 end
 
-private
-	def check_connection ()
-
-	end
-end
